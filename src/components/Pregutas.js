b2c781a5752a2677ed3a0c8f4aea4da8dc40609a
Jose Ramos <joseramos9619@gmail.com>
2021-07-18 23:13:24 -0500

    ajustes de guardado

--- src/components/Pregutas.js
+++ src/components/Pregutas.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react'
 import Contenido from './Contenido';
 class Preguntas extends Component {
 
+    constructor(props) {
+        super(props)
+        this.solucion = React.createRef()
+    }
+
+    fin = () => {
+        if (this.solucion.current.value !== "") {
+            console.log(this.props.cont-1)
+            this.props.guar(this.props.cont-1,this.solucion.current.value);
+            this.props.sig();
+            this.solucion.current.value="";
+        }
+        else {
+            alert("por favor responder la pregunta")
+        }
+    }
+
     render() {
+        let boton = "Sig"
+        let cont = this.props.cont
+        if (this.props.cont === 8) {
+            boton = "Fin"
+        }
+
         return (
             <div className="responder">
                 <div>
-                    <Contenido cont = {this.props.cont}/>
-                    <input type="text" placeholder="respuesta" />
+                    <Contenido cont={cont} />
+                    <input type="text" placeholder="Solución" ref={this.solucion} />
                 </div>
                 <span className="botones">
                     <button onClick={this.props.ant}>Ant</button>
-                    <span className="contador">{this.props.cont}/8</span>
-                    <button onClick={this.props.sig}>Sig</button>
+                    <span className="contador">{cont}/8</span>
+                    <button onClick={this.fin}>{boton}</button>
                 </span>
             </div>
         );
--- src/components/Test.js
+++ src/components/Test.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import Question from './Question'
 import Preguntas from './Pregutas'
 
+let respuestas = ['', '', '', '', '', '', '', ''];
+
 class Test extends Component {
+
 
     constructor(props) {
 
@@ -10,10 +13,14 @@ class Test extends Component {
 
         this.state = {
             contador: 1,
-            respuestas: [],
             solucion: ['a', 'b', 'c', 'c', 'c', 'a', 'c', 'b']
         }
     }
+
+    guadar = (cont, val) => {
+        respuestas[cont] = val
+    }
+
     Aumentacont = () => {
         if (this.state.contador < 8) {
             this.setState({
@@ -29,18 +36,64 @@ class Test extends Component {
         }
     }
     render() {
+        let r = 0;
+        console.log("r" + r)
+        for (let i = 0; i < respuestas.length; i + 1) {
+            let aux = 0;
+            if (respuestas[i] !== "") {
+                aux++;
+            }
+            r = aux
+        }
 
-        return (
-            <React.StrictMode>
-                <main>
-                    <Question cont={this.state.contador} />
-                    <div className="progress">
-                        <progress id="cargar" max="8" value={this.state.contador}></progress>
+        if (r < 8) {
+            console.log(respuestas)
+            return (
+                <React.StrictMode>
+                    <main>
+                        <Question cont={this.state.contador} />
+                        <div className="progress">
+                            <progress id="cargar" max="8" value={this.state.contador}></progress>
+                        </div>
+                        <Preguntas guar={this.guadar} cont={this.state.contador} sol={this.state.respuestas} ant={this.Diminuircont} sig={this.Aumentacont} />
+                    </main>
+                </React.StrictMode>
+            )
+        }
+        else {
+            console.log(respuestas)
+            return (
+                <div className="resultado">
+                    <h2>Resultados</h2>
+                    <span id="rtest"></span>
+                    <div id="res-1">
+                        Respuesta correcta: 1. a) En la boca
                     </div>
-                    <Preguntas cont={this.state.contador} ant={this.Diminuircont} sig={this.Aumentacont}/>
-                </main>
-            </React.StrictMode>
-        )
+                    <div id="res-2">
+                        Respuesta correcta: 2.  b)	El esófago
+                    </div>
+                    <div id="res-3">
+                        Respuesta correcta: 3. c)	Intestino delgado
+                    </div>
+                    <div id="res-4">
+                        Respuesta correcta: 4. c) Intestino grueso
+                    </div>
+                    <div id="res-5">
+                        Respuesta correcta: 5. c) Nacen, crecen, se reproducen y mueren
+                    </div>
+                    <div id="res-6">
+                        Respuesta correcta: 6. a)	Raiz
+                    </div>
+                    <div id="res-7">
+                        Respuesta correcta: 7. c)	Flores, fruto, tallo, hojas y raíz
+                    </div>
+                    <div id="res-8">
+                        Respuesta correcta: 8. b)	El tronco
+                    </div>
+                    <button id="salir-2">Salir</button>
+                </div>
+            )
+        }
     }
 }
 
